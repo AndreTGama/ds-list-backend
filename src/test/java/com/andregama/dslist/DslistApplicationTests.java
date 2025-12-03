aa80e2ef1ae66b2cf9c232e468ecff0ebcf711b5
Andre Gama <andre_gama789@hotmail.com>
2025-12-03 20:30:15 -0300

    first commit

---
+++ src/test/java/com/andregama/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andregama.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
