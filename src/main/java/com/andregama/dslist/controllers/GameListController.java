159827f1ecf1da201b4d47de9e7fc2f98228f577
Andre T Gama <andre_gama789@hotmail.com>
2026-02-02 20:59:58 -0300

    remove lines

--- src/main/java/com/andregama/dslist/controllers/GameListController.java
+++ src/main/java/com/andregama/dslist/controllers/GameListController.java
@@ -1,10 +1,8 @@
 package com.andregama.dslist.controllers;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
